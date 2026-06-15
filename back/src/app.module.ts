b12c28798c06eb6ca60ab99c47dae14f75af3b34
Nicolas Nguyen <nicolasnguyen@mac.home>
2026-06-15 14:41:52 +0200

    ✨(add): add prisma client

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [TerminusModule, HttpModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TerminusModule,
+    HttpModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
